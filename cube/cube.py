72360702738dedca27d15aa4e7943702e9de4497
pgj92 <pgjanus@gmail.com>
2026-05-16 22:11:04 +0200

    Implement face rotations

--- cube/cube.py
+++ cube/cube.py
@@ -11,9 +11,9 @@ straight at that face:
          3 4 5         <- U face (indices 0..8 in the state array)
          6 7 8
 
-  9 10 11  18 19 20  27 28 29  36 37 38
-  12 13 14 21 22 23  30 31 32  39 40 41   <- L, F, R, B faces
-  15 16 17 24 25 26  33 34 35  42 43 44      (indices 9..44)
+   9 10 11  18 19 20  27 28 29  36 37 38
+  12 13 14  21 22 23  30 31 32  39 40 41   <- L, F, R, B faces
+  15 16 17  24 25 26  33 34 35  42 43 44      (indices 9..44)
 
         45 46 47
         48 49 50       <- D face (indices 45..53)
@@ -112,30 +112,209 @@ def _build_U_perm() -> np.ndarray:
     return perm
 
 
-# === TODO: Generative Task #2 ===
-# Build the permutation for each of the remaining 5 face moves.
-# Use _build_U_perm above as your template. You'll need to:
-#   1. Rotate the face's own 9 stickers CW.
-#   2. Identify the four 3-sticker strips on adjacent faces that get cycled.
-#   3. Wire up the cycle.
-#
-# Hint: drawing the cube on paper and labeling sticker indices is by far
-# the fastest way. Don't try to do it in your head.
-
 def _build_D_perm() -> np.ndarray:
-    raise NotImplementedError("Generative Task #2: implement D move")
+    """
+    Build the permutation array for a single D (down, 90° CW) move.
+
+    A D move:
+      - rotates the 9 stickers on the D face clockwise
+      - cycles the BOTTOM ROW of the four side faces:
+            F bottom row -> L bottom row
+            L botton row -> B bottom row
+            B botton row -> R bottom row
+            R botton row -> F bottom row
+        (when viewed from top, the bottom edges move CCW)
+
+    Bottom rows are:
+        L: indices 15, 16, 17
+        F: indices 24, 25, 26
+        R: indices 33, 34, 35
+        B: indices 42, 43, 44
+    """
+    perm = _identity_perm()
+
+    # Rotate D face stickers CW (D face starts at 45)
+    _rotate_face_cw(perm, 45)
+
+    # Cycle the bottom rows: F -> R -> B -> L -> F
+    # perm[i] tells us "where to read the new value from"
+    # So if F's bottom row goes TO R's bottom row, then new R bottom row reads from old F bottom row:
+    #     perm[R_bottom[k]] = F_bottom[k]
+    L_BOTTOM = [15, 16, 17]
+    F_BOTTOM = [24, 25, 26]
+    R_BOTTOM = [33, 34, 35]
+    B_BOTTOM = [42, 43, 44]
+
+    for k in range(3):
+        perm[R_BOTTOM[k]] = F_BOTTOM[k]
+        perm[B_BOTTOM[k]] = R_BOTTOM[k]
+        perm[L_BOTTOM[k]] = B_BOTTOM[k]
+        perm[F_BOTTOM[k]] = L_BOTTOM[k]
+
+    return perm
+
 
 def _build_L_perm() -> np.ndarray:
-    raise NotImplementedError("Generative Task #2: implement L move")
+    """
+    Build the permutation array for a single L (left, 90° CW) move.
+
+    A L move:
+      - rotates the 9 stickers on the L face clockwise
+      - cycles one COLUMN of the four side faces:
+            U left column -> F left column
+            F left column -> D left column
+            D left column (reversed!) -> B right! column 
+            B right! column (reversed!) -> U left column
+            
+
+    Rotated columns are:
+        U: indices 0, 3, 6
+        F: indices 9, 12, 15
+        D: indices 45, 48, 51
+        B: indices 38, 41, 44
+    """
+    perm = _identity_perm()
+
+    # Rotate L face stickers CW (L face starts at 9)
+    _rotate_face_cw(perm, 9)
+
+    # Cycle the columns: U -> F -> D -(r)-> B -(r)-> U
+    # perm[i] tells us "where to read the new value from"
+    # So if F's left column goes to D's left column, then new D left column reads from old F left column:
+    #     perm[D_left[k]] = F_left[k]
+    # For rotations with B's right column indices are reversed, thus:
+    #     perm[B_right[k]] = D_left[::-1][k]
+    U_LEFT = [0, 3, 6]
+    F_LEFT = [18, 21, 24]
+    D_LEFT = [45, 48, 51]
+    B_RIGHT = [38, 41, 44]
+
+    for k in range(3):
+        perm[F_LEFT[k]] = U_LEFT[k]
+        perm[D_LEFT[k]] = F_LEFT[k]
+        perm[B_RIGHT[k]] = D_LEFT[::-1][k]
+        perm[U_LEFT[k]] = B_RIGHT[::-1][k]
+
+    return perm
+
 
 def _build_R_perm() -> np.ndarray:
-    raise NotImplementedError("Generative Task #2: implement R move")
+    """
+    Build the permutation array for a single R (right, 90° CW) move.
+
+    A R move:
+      - rotates the 9 stickers on the R face clockwise
+      - cycles one COLUMN of the four side faces:
+            U right column (reversed!) -> B left! column
+            B left! column (reversed!) -> D right column
+            D right column -> F right column
+            F right column -> U right column
+
+    Rotated columns are:
+        U: indices 2, 5, 8
+        B: indices 36, 39, 42
+        D: indices 47, 50, 53
+        F: indices 20, 23, 26
+    """
+    perm = _identity_perm()
+
+    # Rotate R face stickers CW (R face starts at 27)
+    _rotate_face_cw(perm, 27)
+
+    # Cycle the columns: U -(r)-> B -(r)-> D -> F -> U
+    # perm[i] tells us "where to read the new value from"
+    # So if D's right column goes to F's right column, then new F right column reads from old D right column:
+    #     perm[F_right[k]] = D_right[k]
+    # For rotations with B's left column indices are reversed, thus:
+    #     perm[B_left[k]] = U_right[::-1][k]
+    U_RIGHT = [2, 5, 8]
+    B_LEFT = [36, 39, 42]
+    D_RIGHT = [47, 50, 53]
+    F_RIGHT = [20, 23, 26]
+
+    for k in range(3):
+        perm[B_LEFT[k]] = U_RIGHT[::-1][k]
+        perm[D_RIGHT[k]] = B_LEFT[::-1][k]
+        perm[F_RIGHT[k]] = D_RIGHT[k]
+        perm[U_RIGHT[k]] = F_RIGHT[k]
+
+    return perm
+
 
 def _build_F_perm() -> np.ndarray:
-    raise NotImplementedError("Generative Task #2: implement F move")
+    """
+    Build the permutation array for a single F (front, 90° CW) move.
+
+    A F move:
+    - rotates the 9 stickers on the F face clockwise
+    - cycles rows and columns of four faces:
+        U bottom row -> R left column
+        R left column -> D top row (reversed!)
+        D top row -> L right column
+        L right column -> U bottom row (reversed!)
+
+    Rotated rows and columns are:
+        U indices: 6, 7, 8
+        R indices: 27, 39, 33
+        D indices: 45, 46, 47
+        L indices: 11, 14, 17
+    """
+    perm = _identity_perm()
+
+    # Rotate F face stickers CW.
+    _rotate_face_cw(perm, 18)
+
+    # Cycle rows and columns: U -> R -(r)-> D -> L -(r)-> U
+    U_BOTTOM = [6, 7, 8]
+    R_LEFT = [27, 39, 33]
+    D_TOP = [45, 46, 47]
+    L_RIGHT = [11, 14, 17]
+
+    for k in range(3):
+        perm[R_LEFT[k]] = U_BOTTOM[k]
+        perm[D_TOP[k]] = R_LEFT[::-1][k]
+        perm[L_RIGHT[k]] = D_TOP[k]
+        perm[U_BOTTOM[k]] = L_RIGHT[::-1][k]
+
+    return perm
+
 
 def _build_B_perm() -> np.ndarray:
-    raise NotImplementedError("Generative Task #2: implement B move")
+    """
+    Build the permutation array for a single B (back, 90° CW) move.
+
+    A B move:
+    - rotates the 9 stickers on the B face clockwise
+    - cycles rows and columns of four faces:
+        U top row -> L left column (reversed!)
+        L left column -> D bottom row
+        D bottom row -> R right column (reversed!)
+        R right column -> U top row
+
+    Rotated rows and columns are:
+        U indices: 0, 1, 2
+        L indices: 9, 12, 15
+        D indices: 51, 52, 53
+        R indices: 29, 32, 35
+    """
+    perm = _identity_perm()
+
+    # Rotate B face stickers CW.
+    _rotate_face_cw(perm, 36)
+
+    # Cycle rows and columns: U -(r)-> L -> D -(r)-> R -> U
+    U_TOP = [0, 1, 2]
+    L_LEFT = [9, 12, 15]
+    D_BOTTOM = [51, 52, 53]
+    R_RIGHT = [29, 32, 35]
+
+    for k in range(3):
+        perm[L_LEFT[k]] = U_TOP[::-1][k]
+        perm[D_BOTTOM[k]] = L_LEFT[k]
+        perm[R_RIGHT[k]] = D_BOTTOM[::-1][k]
+        perm[U_TOP[k]] = R_RIGHT[k]
+
+    return perm
 
 
 _BASE_BUILDERS = {
